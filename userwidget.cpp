f601051b1d61c1a76f760c20322091c32fb8bc09
Djivs <icemcpe099@gmail.com>
2023-02-28 22:13:57 +0300

    userwidget front

--- userwidget.cpp
+++ userwidget.cpp
@@ -1,7 +1,23 @@
 #include "userwidget.h"
 UserWidget::UserWidget(QWidget *parent) :
     QWidget{parent} {
+    title = new QLabel("User menu");
 
+    chessplayersStats = new QPushButton("Chessplayers stats");
+    openingsStats = new QPushButton("Openings stats");
+    tournamentsStats = new QPushButton("Tournaments stats");
+    gamesList = new QPushButton("Chess games list");
+    back = new QPushButton("Go back");
+
+    layout = new QVBoxLayout;
+    layout->addWidget(title);
+    layout->addWidget(chessplayersStats);
+    layout->addWidget(openingsStats);
+    layout->addWidget(tournamentsStats);
+    layout->addWidget(gamesList);
+    layout->addWidget(back);
+
+    setLayout(layout);
 }
 
 UserWidget::~UserWidget() {
--- userwidget.h
+++ userwidget.h
@@ -2,11 +2,22 @@
 #define USERWIDGET_H
 
 #include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+#include <QVBoxLayout>
 
 class UserWidget : public QWidget {
     Q_OBJECT
 public:
     explicit UserWidget(QWidget *parent = nullptr);
     ~UserWidget();
+private:
+    QLabel *title;
+    QPushButton *chessplayersStats;
+    QPushButton *openingsStats;
+    QPushButton *tournamentsStats;
+    QPushButton *gamesList;
+    QPushButton *back;
+    QVBoxLayout *layout;
 };
 #endif //USERWIDGET_H
