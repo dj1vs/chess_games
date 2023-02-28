e27a3dc86198f3315f2e0e74492ab90c128f2237
Djivs <icemcpe099@gmail.com>
2023-03-01 00:24:32 +0300

    start userwidgets/chessplayersstatswidget front

--- userwidgets/chessplayersstatswidget.cpp
+++ userwidgets/chessplayersstatswidget.cpp
@@ -1,7 +1,59 @@
 #include "chessplayersstatswidget.h"
 ChessplayersStatsWidget::ChessplayersStatsWidget(QWidget *parent):
     QWidget{parent} {
+        search = new QLineEdit();
+        QLineEdit *name;
 
+        gamesWhite = new QTableView();
+        gamesBlack = new QTableView();
+        openingsWhite = new QTableView();
+        openingsBlack = new QTableView();
+        strongestOponents = new QTableView();
+
+        rating = new QSpinBox();
+        birthYear = new QSpinBox();
+        amount = new QSpinBox();
+        wins = new QSpinBox();
+        loses = new QSpinBox();
+        draws = new QSpinBox();
+        amountWhite = new QSpinBox();
+        winsWhite = new QSpinBox();
+        losesWhite = new QSpinBox();
+        drawsWhite = new QSpinBox();
+        amountBlack = new QSpinBox();
+        winsBlack = new QSpinBox();
+        losesBlack = new QSpinBox();
+        drawsBlack = new QSpinBox();
+        
+        
+        whiteOpeningsGraph = new QWidget();
+        blackOpeningsGraph = new QWidget();
+        
+
+        layout = new QFormLayout();
+        layout->addRow(tr("Search chessplayers by name:"), search);
+        layout->addRow(tr("Name:"), name);
+        layout->addRow(tr("ELO Rating:"), rating);
+        layout->addRow(tr("Birth year:"), birthYear);
+        layout->addRow(tr("Games amount:"), amount);
+        layout->addRow(tr("Wins amount:"), wins);
+        layout->addRow(tr("Loses amount:"), loses);
+        layout->addRow(tr("Draws amount:"), draws);
+        layout->addRow(tr("Games as white:"), gamesWhite);
+        layout->addRow(tr("Games amount as white:"), amountWhite);
+        layout->addRow(tr("Wins as white:"), winsWhite);
+        layout->addRow(tr("Loses as white:"), losesWhite);
+        layout->addRow(tr("Draws as white:"), drawsWhite);
+        layout->addRow(tr("Games as black:"), gamesBlack);
+        layout->addRow(tr("Wins as black:"), winsBlack);
+        layout->addRow(tr("Loses as black:"), losesBlack);
+        layout->addRow(tr("Draws as black:"), drawsBlack);
+        layout->addRow(tr("Openings as white:"), openingsWhite);
+        layout->addRow(tr("Openings as black:"), openingsBlack);
+        layout->addRow(whiteOpeningsGraph, blackOpeningsGraph);
+        layout->addRow(tr("Stronges opponents:"), strongestOponents);
+
+        setLayout(layout);
 }
 
 ChessplayersStatsWidget::~ChessplayersStatsWidget() {
--- userwidgets/chessplayersstatswidget.h
+++ userwidgets/chessplayersstatswidget.h
@@ -2,11 +2,47 @@
 #define CHESSPLAYERSSTATSWIDGET_H
 
 #include <QWidget>
+#include <QLabel>
+#include <QLineEdit>
+#include <QTableView>
+#include <QSpinBox>
+
+#include <QVBoxLayout>
+#include <QFormLayout>
+
 
 class ChessplayersStatsWidget : public QWidget {
     Q_OBJECT
 public:
     explicit ChessplayersStatsWidget(QWidget *parent = nullptr);
     ~ChessplayersStatsWidget();
+private:
+    QLineEdit *search;
+    QLineEdit *name;
+    QSpinBox *rating;
+    QSpinBox *birthYear;
+    QSpinBox *amount;
+    QSpinBox *wins;
+    QSpinBox *loses;
+    QSpinBox *draws;
+    QTableView *gamesWhite;
+    QTableView *gamesBlack;
+    QSpinBox *amountWhite;
+    QSpinBox *winsWhite;
+    QSpinBox *losesWhite;
+    QSpinBox *drawsWhite;
+    QSpinBox *amountBlack;
+    QSpinBox *winsBlack;
+    QSpinBox *losesBlack;
+    QSpinBox *drawsBlack;
+    QTableView *openingsWhite;
+    QTableView *openingsBlack;
+    QWidget *whiteOpeningsGraph;
+    QWidget *blackOpeningsGraph;
+    QTableView *strongestOponents;
+
+
+    QFormLayout *layout;
+
 };
 #endif //CHESSPLAYERSSTATSWIDGET_H
