7cb5451e887050b57162c855c3e7a8183c6cd1e4
Djivs <icemcpe099@gmail.com>
2023-02-28 11:05:08 +0300

    loginwidget: login and password inputs, title

--- loginwidget.cpp
+++ loginwidget.cpp
@@ -4,9 +4,36 @@ LoginWidget::LoginWidget(QWidget *parent)
     : QWidget{parent} {
         loginButton = new QPushButton("Login");
 
+        loginLineEdit = new QLineEdit;
+        passwordLineEdit = new QLineEdit;
+        passwordLineEdit->setEchoMode(QLineEdit::Password);
+
+        loginLabel = new QLabel("Login:");
+        passwordLabel = new QLabel("Password:");
+        titleLabel = new QLabel("АСС Шахматные партии");
+
+        loginLayout = new QHBoxLayout();
+        loginLayout->addWidget(loginLabel);
+        loginLayout->addWidget(loginLineEdit);
+
+        passwordLayout = new QHBoxLayout();
+        passwordLayout->addWidget(passwordLabel);
+        passwordLayout->addWidget(passwordLineEdit);
+
         layout = new QVBoxLayout;
+        layout->addWidget(titleLabel);
+        layout->addLayout(loginLayout);
+        layout->addLayout(passwordLayout);
         layout->addWidget(loginButton);
 
         setLayout(layout);
         
+}
+
+LoginWidget::~LoginWidget() {
+    delete loginButton;
+    delete loginLineEdit, loginLabel, loginLayout;
+    delete passwordLabel, passwordLayout, passwordLineEdit;
+    delete titleLabel;
+    delete layout;
 }
--- loginwidget.h
+++ loginwidget.h
@@ -3,16 +3,28 @@
 
 #include <QWidget>
 #include <QPushButton>
+#include <QLineEdit>
+#include <QLabel>
+
+#include <QHBoxLayout>
 #include <QVBoxLayout>
 
 class LoginWidget : public QWidget {
     Q_OBJECT
 public:
     explicit LoginWidget(QWidget *parent = nullptr);
+    ~LoginWidget();
 
 private:
     QPushButton *loginButton;
+    QLineEdit *loginLineEdit;
+    QLineEdit *passwordLineEdit;
+    QLabel *loginLabel;
+    QLabel *passwordLabel;
+    QLabel *titleLabel;
 
+    QHBoxLayout *loginLayout;
+    QHBoxLayout *passwordLayout;
     QVBoxLayout *layout;
 
 signals:
