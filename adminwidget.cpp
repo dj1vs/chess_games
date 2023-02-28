06599e430011808568a9ff666dcefe1a75f040b5
Djivs <icemcpe099@gmail.com>
2023-02-28 22:20:53 +0300

    adminwidget front

--- adminwidget.cpp
+++ adminwidget.cpp
@@ -1,6 +1,26 @@
 #include "adminwidget.h"
 AdminWidget::AdminWidget(QWidget *parent):
-    QWidget(parent) {
+    QWidget{parent} {
+    title = new QLabel("Admin menu");
+    chessplayers = new QPushButton("Chessplayers");
+    openings = new QPushButton("Openings");
+    tournaments = new QPushButton("Tournaments");
+    places = new QPushButton("Places");
+    judges = new QPushButton("Judges");
+    games = new QPushButton("Chess games");
+    back = new QPushButton("Go back");
+
+    layout = new QVBoxLayout();
+    layout->addWidget(title);
+    layout->addWidget(chessplayers);
+    layout->addWidget(openings);
+    layout->addWidget(tournaments);
+    layout->addWidget(places);
+    layout->addWidget(judges);
+    layout->addWidget(games);
+    layout->addWidget(back);
+
+    setLayout(layout);
 
 }
 
--- adminwidget.h
+++ adminwidget.h
@@ -2,12 +2,30 @@
 #define ADMINWIDGET_H
 
 #include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+
+#include <QVBoxLayout>;
 
 class AdminWidget : public QWidget {
     Q_OBJECT
 public:
     explicit AdminWidget(QWidget *parent = nullptr);
     ~AdminWidget();
+
+
+private:
+    QLabel *title;
+    QPushButton *chessplayers;
+    QPushButton *openings;
+    QPushButton *tournaments;
+    QPushButton *places;
+    QPushButton *judges;
+    QPushButton *games;
+    QPushButton *back;
+
+    QVBoxLayout *layout;
+
 };
 
 
