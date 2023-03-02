3740eb82eeefb64c7a3b0394ed509983284a7bf8
Djivs <icemcpe099@gmail.com>
2023-03-02 15:34:35 +0300

    chessplayerswidget design

--- adminwidgets/chessplayerswidget.cpp
+++ adminwidgets/chessplayerswidget.cpp
@@ -1,7 +1,26 @@
 #include "chessplayerswidget.h"
 ChessplayersWidget::ChessplayersWidget(QWidget *parent):
     QWidget{parent} {
+    formHeader = new FormHeader;
+    formHeader->setTitle("Chessplayers");
 
+    pageLayout = new QFormLayout;
+
+    id = new QComboBox;
+    name = new QLineEdit;
+    rating = new QComboBox;
+    birthYear = new QComboBox;
+
+    pageLayout->addRow("Chessplayer id", id);
+    pageLayout->addRow("Name", name);
+    pageLayout->addRow("Rating", rating);
+    pageLayout->addRow("Birth year", birthYear);
+    
+    mainLayout = new QVBoxLayout;
+    mainLayout->addWidget(formHeader);
+    mainLayout->addLayout(pageLayout);
+
+    setLayout(mainLayout);
 }
 
 ChessplayersWidget::~ChessplayersWidget() {
--- adminwidgets/chessplayerswidget.h
+++ adminwidgets/chessplayerswidget.h
@@ -1,12 +1,29 @@
 #ifndef CHESSPLAYERSWIDGET_H
 #define CHESSPLAYERSWIDGET_H
 
+#include "../formheader.h"
+
 #include <QWidget>
+
+#include <QLineEdit>
+#include <QComboBox>
+
+#include <QFormLayout>
+#include <QVBoxLayout>
 
 class ChessplayersWidget : public QWidget {
     Q_OBJECT
 public:
     explicit ChessplayersWidget(QWidget *parent = nullptr);
     ~ChessplayersWidget();
+private:
+    FormHeader *formHeader;
+
+    QLineEdit *name;
+    QComboBox *id, *rating, *birthYear;
+
+
+    QFormLayout *pageLayout;
+    QVBoxLayout *mainLayout;
 };
 #endif //CHESSPLAYERSWIDGET_H
