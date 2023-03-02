2ed08af8cc651fdab58385cb29ba618d7c03be18
Djivs <icemcpe099@gmail.com>
2023-03-02 15:41:08 +0300

    openingswidget front

--- adminwidgets/openingswidget.cpp
+++ adminwidgets/openingswidget.cpp
@@ -1,6 +1,32 @@
 #include "openingswidget.h"
 OpeningsWidget::OpeningsWidget(QWidget *parent):
     QWidget{parent} {
+    formHeader = new FormHeader;
+    formHeader->setTitle("Openings");
+
+    pageLayout = new QFormLayout;
+
+    id = new QLineEdit();
+    group = new QLineEdit;
+    name = new QLineEdit;
+    group = new QLineEdit;
+    namedAfter = new QLineEdit;
+    altNames = new QLineEdit;
+    moves = new QLineEdit;
+
+    pageLayout->addRow("ECO ID", id);
+    pageLayout->addRow("Group", group);
+    pageLayout->addRow("Name", name);
+    pageLayout->addRow("Moves", moves);
+    pageLayout->addRow("Alternative names", altNames);
+    pageLayout->addRow("Named after", namedAfter);
+    
+
+    mainLayout = new QVBoxLayout;
+    mainLayout->addWidget(formHeader);
+    mainLayout->addLayout(pageLayout);
+
+    setLayout(mainLayout);
 
 }
 
--- adminwidgets/openingswidget.h
+++ adminwidgets/openingswidget.h
@@ -1,12 +1,26 @@
 #ifndef OPENINGSWIDGET_H
 #define OPENINGSWIDGET_H
 
+#include "../formheader.h"
+
 #include <QWidget>
+
+#include <QLineEdit>
+
+#include <QFormLayout>
+#include <QVBoxLayout>
 
 class OpeningsWidget : public QWidget {
     Q_OBJECT
 public:
     explicit OpeningsWidget(QWidget *parent = nullptr);
-    ~OpeningsWidget();
+   ~OpeningsWidget();
+private:
+    FormHeader *formHeader;
+
+    QLineEdit *id, *group, *name, *moves, *altNames, *namedAfter;
+
+    QFormLayout *pageLayout;
+    QVBoxLayout *mainLayout;
 };
 #endif //OPENINGSWIDGET_H
