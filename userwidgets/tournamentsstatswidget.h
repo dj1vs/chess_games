b836ef0c405789224b63b82a274011f5d7b2dc15
dmitriy <icemcpe099@gmail.com>
2023-03-02 11:47:04 +0300

    start tournaments stats widget front develop:

--- userwidgets/tournamentsstatswidget.h
+++ userwidgets/tournamentsstatswidget.h
@@ -3,10 +3,21 @@
 
 #include <QWidget>
 
+#include <QLineEdit>
+#include <QLabel>
+#include <QPushButton>
+#include <QTableView>
+
+#include <QtCharts>
+
+using namespace QtCharts;
 class TournamentsStatsWidget : public QWidget {
     Q_OBJECT
 public:
     explicit TournamentsStatsWidget(QWidget *parent = nullptr);
     ~TournamentsStatsWidget();
+private:
+    QLineEdit *search, *name, *winnersName, *judgesName, *country, *city;
+
 };
 #endif //TOURNAMENTSSTATSWIDGET_H
