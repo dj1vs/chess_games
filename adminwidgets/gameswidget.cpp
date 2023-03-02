b4b44c93badf4cfa9f382ec49291d6a7d90b4885
Djivs <icemcpe099@gmail.com>
2023-03-02 16:27:04 +0300

    gameswidget front

--- adminwidgets/gameswidget.cpp
+++ adminwidgets/gameswidget.cpp
@@ -1,7 +1,34 @@
 #include "gameswidget.h"
 GamesWidget::GamesWidget(QWidget *parent):
     QWidget{parent} {
+    formHeader = new FormHeader;
+    formHeader->setTitle("Chess games");
 
+    id = new QComboBox;
+    format = new QLineEdit;
+    moves = new QTextBrowser;
+    result = new QComboBox;
+    timeControl = new QLineEdit;
+    date = new QLineEdit;
+    white = new QLineEdit;
+    black = new QLineEdit;
+    opening = new QComboBox;
+    tournamet = new QComboBox;
+
+    layout = new QFormLayout;
+    layout->addWidget(formHeader);
+    layout->addRow("ID", id);
+    layout->addRow("Format", format);
+    layout->addRow("Moves", moves);
+    layout->addRow("Result", result);
+    layout->addRow("Time control", timeControl);
+    layout->addRow("Date", date);
+    layout->addRow("White", white);
+    layout->addRow("Black", black);
+    layout->addRow("Opening", opening);
+    layout->addRow("Tournament", tournamet);
+
+    setLayout(layout);
 }
 
 GamesWidget::~GamesWidget() {
--- adminwidgets/gameswidget.h
+++ adminwidgets/gameswidget.h
@@ -1,12 +1,28 @@
 #ifndef GAMESWIDGET_H
 #define GAMESWIDGET_H
-    
+
+#include "../formheader.h"
+
 #include <QWidget>
+
+#include <QLineEdit>
+#include <QComboBox>
+#include <QTextBrowser>
+#include <QFormLayout>
+
 
 class GamesWidget : public QWidget {
     Q_OBJECT
 public:
     explicit GamesWidget(QWidget *parent = nullptr);
     ~GamesWidget();
+private:
+    FormHeader *formHeader;
+    QComboBox *id, *result, *opening, *tournamet;
+    QLineEdit *format, *timeControl, *date, *white, *black, ;
+    QTextBrowser *moves;
+
+    QFormLayout *layout;
+
 };
 #endif //GAMESWIDGET_H
