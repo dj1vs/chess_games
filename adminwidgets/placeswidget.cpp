141f4916de4416d91d29cdcb338c2ddcfa86aa11
Djivs <icemcpe099@gmail.com>
2023-03-02 16:12:16 +0300

    placeswidget front

--- adminwidgets/placeswidget.cpp
+++ adminwidgets/placeswidget.cpp
@@ -2,6 +2,31 @@
 PlacesWidget::PlacesWidget(QWidget *parent):
     QWidget{parent} {
 
+    city = new QLineEdit;
+    country = new QLineEdit;
+    id = new QComboBox;
+    formHeader = new FormHeader;
+
+    pageLayout = new QFormLayout;
+    pageLayout->addRow("Places", gotoTournaments);
+    pageLayout->addRow("ID", id);
+    pageLayout->addRow("City", city);
+    pageLayout->addRow("Country",country);
+    pageLayout->addWidget(formHeader);
+
+    setLayout(pageLayout);
+
+    connect(gotoTournaments, &QPushButton::clicked, this, [this] {
+        placesTournaments = new QTableView;
+        QVBoxLayout *layout = new QVBoxLayout;
+        layout->addWidget(placesTournaments);
+        layout->addWidget(formHeader);
+
+        delete pageLayout;
+        setLayout(layout);
+    });
+
+
 }
 
 PlacesWidget::~PlacesWidget() {
--- adminwidgets/placeswidget.h
+++ adminwidgets/placeswidget.h
@@ -1,12 +1,36 @@
 #ifndef PLACESWIDGET_H
 #define PLACESWIDGET_H
 
+#include "../formheader.h"
 #include <QWidget>
+#include <QPushButton>
+#include <QLabel>
+#include <QLineEdit>
+#include <QTableView>
+#include <QComboBox>
+
+#include <QFormLayout>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 
 class PlacesWidget : public QWidget {
     Q_OBJECT
 public:
     explicit PlacesWidget(QWidget *parent = nullptr);
     ~PlacesWidget();
+private:
+    FormHeader *formHeader;
+    QLineEdit *city, *country;
+    QComboBox *id;
+    QPushButton *gotoTournaments;
+
+    QTableView *placesTournaments;
+
+    QHBoxLayout *placesHeader;
+    QVBoxLayout *mainLayout;
+    QFormLayout *pageLayout;
+
+    
+
 };
 #endif //PLACESWIDGET_H
