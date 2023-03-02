7f668a044fba2e4baaf686bde542131260349051
Djivs <icemcpe099@gmail.com>
2023-03-02 15:56:03 +0300

    tournamentswidget front

--- adminwidgets/tournamentswidget.cpp
+++ adminwidgets/tournamentswidget.cpp
@@ -1,7 +1,40 @@
 #include "tournamentswidget.h"
 TournamentsWidget::TournamentsWidget(QWidget *parent):
     QWidget{parent} {
+    formHeader = new FormHeader;
+    formHeader->setTitle("Tournaments");
 
+    pageLayout = new QGridLayout;
+    id = new QComboBox;
+    name = new QLineEdit;
+    ratingRestriction = new QComboBox;
+    winner = new QLineEdit;
+    city = new QLineEdit;
+    country = new QLineEdit;
+    judgeBox = new QGroupBox;
+    playedGames = new QTableView;
+
+    pageLayout->addWidget(new QLabel("ID"), 0, 0, 1, 1);
+    pageLayout->addWidget(id, 0, 1, 1, 1);
+    pageLayout->addWidget(new QLabel("Tournaments name"), 1, 0, 1, 1);
+    pageLayout->addWidget(id, 1, 1, 1, 1);
+    pageLayout->addWidget(new QLabel("Rating restriction"), 2, 0, 1, 1);
+    pageLayout->addWidget(id, 2, 1, 1, 1);
+    pageLayout->addWidget(new QLabel("Winner"), 3, 0, 1, 1);
+    pageLayout->addWidget(id, 3, 1, 1, 1);
+    pageLayout->addWidget(new QLabel("City"), 4, 0, 1, 1);
+    pageLayout->addWidget(id, 4, 1, 1, 1);
+    pageLayout->addWidget(new QLabel("Country"), 4, 2, 1, 1);
+    pageLayout->addWidget(id, 4, 3, 1, 1);
+    pageLayout->addWidget(judgeBox, 5, 0, 1, 1);
+    pageLayout->addWidget(new QLabel("Games, played at a tournament:"), 6, 0, 1, 1);
+    pageLayout->addWidget(playedGames, 6, 1, 1, 1);
+
+    mainLayout = new QVBoxLayout;
+    mainLayout->addWidget(formHeader);
+    mainLayout->addLayout(pageLayout);
+
+    setLayout(mainLayout);
 }
 
 TournamentsWidget::~TournamentsWidget() {
--- adminwidgets/tournamentswidget.h
+++ adminwidgets/tournamentswidget.h
@@ -1,12 +1,35 @@
 #ifndef TOURNAMENTSWIDGET_H
 #define TOURNAMENTSWIDGET_H
 
+#include "../formheader.h"
+
 #include <QWidget>
+#include <QGridLayout>
+#include <QVBoxLayout>
+#include <QLineEdit>
+#include <QTableView>
+#include <QGroupBox>
+#include <QRadioButton>
+#include <QComboBox>
+#include <QVector>
+#include <QLabel>
 
 class TournamentsWidget : public QWidget {
     Q_OBJECT
 public:
     explicit TournamentsWidget(QWidget *parent = nullptr);
     ~TournamentsWidget();
+private:
+    FormHeader *formHeader;
+    QComboBox *id, *ratingRestriction;
+    QLineEdit *name, *winner, *city, *country;
+    QGroupBox *judgeBox;
+    QVector <QRadioButton*> judgeButtons;
+    QTableView *playedGames;
+
+    QGridLayout *pageLayout;
+    QVBoxLayout *mainLayout;
+     
+    
 };
 #endif //TOURNAMENTSWIDGET_H
