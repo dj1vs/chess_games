65eb42f95fecb6f8a113f33d9531ba54582b9426
Djivs <icemcpe099@gmail.com>
2023-03-04 21:16:07 +0300

    finish basics

--- adminwidgets/judgeswidget.cpp
+++ adminwidgets/judgeswidget.cpp
@@ -1,4 +1,6 @@
 #include "judgeswidget.h"
+
+#include <QSqlQueryModel>
 JudgesWidget::JudgesWidget(FormWidget *parent):
     FormWidget{parent} {
     formHeader = new FormHeader;
@@ -11,14 +13,14 @@ JudgesWidget::JudgesWidget(FormWidget *parent):
     tournaments = new QTableView;
 
     layout = new QGridLayout;
-    layout->addWidget(formHeader);
-    layout->addWidget(new QLabel("ID"));
-    layout->addWidget(id);
-    layout->addWidget(new QLabel("Name"));
-    layout->addWidget(name);
-    layout->addWidget(new QLabel("Mail"));
-    layout->addWidget(mail);
-    layout->addWidget(tournaments);
+    layout->addWidget(formHeader, 0, 0, 1, 1);
+    layout->addWidget(new QLabel("ID"), 1, 0, 1, 1);
+    layout->addWidget(id, 2, 0, 1, 1);
+    layout->addWidget(new QLabel("Name"), 3, 0, 1, 1);
+    layout->addWidget(name, 4, 0, 1, 1);
+    layout->addWidget(new QLabel("Mail"), 5, 0, 1, 1);
+    layout->addWidget(mail, 6, 0, 1, 1);
+    layout->addWidget(tournaments, 7, 0, 2, 5);
 
     setLayout(layout);
 
@@ -32,12 +34,25 @@ JudgesWidget::~JudgesWidget() {
 }
 
 void JudgesWidget::loadPage() {
+    id->setValue(curInd);
     QSqlQuery query("SELECT name, email FROM judges WHERE judge_id = " + QString::number(curInd));
 
     while (query.next()) {
         name->setText(query.value(0).toString());
         mail->setText(query.value(1).toString());
     }
+
+    QSqlQueryModel *model = new QSqlQueryModel;
+    model->setQuery("SELECT tournaments.name, winner.name, city, country"
+                      " FROM tournaments"
+                      " INNER JOIN chessplayers AS winner ON winner.chessplayer_id = winner_id"
+                      " INNER JOIN places ON places.place_id = tournaments.place_id"
+                      " WHERE judge_id = " + QString::number(curInd));
+
+    tournaments->setModel(model);
+    tournaments->show();
+
+
 
 
 }
